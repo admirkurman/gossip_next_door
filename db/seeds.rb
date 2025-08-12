0da5c4f84c35c35d82e0b17216692b620573b7f4
exec zsh <admir.kurman@gmail.com>
2025-08-12 17:18:37 +0100

    Filled out the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+community = Community.new(title: "Default Community", description: "This is the default community for the application.")
+community.save!
+
+# Create a default user associated with the community
+User.create!(
+  email: "admir@admir.com",
+  password: "password",
+  password_confirmation: "password",
+  username: "admir",
+  forename: "Admir",
+  surname: "Kurman",
+  shortbio: "A passionate developer",
+  community: community
+)
+
+# Create a default post associated with the user
+Post.create!(
+  title: "Welcome to the Community",
+  body: "This is the first post in the default community. Feel free to explore and contribute!",
+  user: User.first
+)
